ec6f76c5f19951c16f96c31d5afed5848b630b86
duckysayhello <140191463+duckysayhello@users.noreply.github.com>
2024-04-10 10:04:58 +0700

    Add files via upload

---
+++ LTNC_01/5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d)
+{
+    int maxx=0;
+    maxx=max(maxx,a);
+    maxx=max(maxx,b);
+    maxx=max(maxx,c);
+    maxx=max(maxx,d);
+    return maxx;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
